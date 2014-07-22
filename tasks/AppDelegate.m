bb713dbc1392d3d61f30010e036a986923e106d2
Nicolas Miyasato <nmiyasato@gmail.com>
2014-07-22 15:10:20 -0300

    Updating to latest sdk

--- tasks/AppDelegate.m
+++ tasks/AppDelegate.m
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <KZApplication.h>
 
 #define TENANT  @"https://marketplace.kidocloud.com"
 #define APP     @"tasks"
@@ -10,13 +11,24 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
-
-    self.kidozenApplication = [[KZApplication alloc] initWithTennantMarketPlace:TENANT applicationName:APP bypassSSLValidation:YES andCallback:^(KZResponse * kr) {
-        NSAssert(!kr.error, @"error must be null");
-        [kr.application authenticateUser:USER withProvider:@"Kidozen" andPassword:PASS completion:^(id kr) {
+    __weak AppDelegate *safeMe = self;
+    
+    self.kidozenApplication = [[KZApplication alloc] initWithTenantMarketPlace:TENANT
+                                                               applicationName:APP
+                                                                applicationKey:APPLICATION_KEY
+                                                                     strictSSL:NO
+                                                                   andCallback:^(KZResponse *response)
+    {
+        NSAssert(!response.error, @"error must be null");
+        [safeMe.kidozenApplication authenticateUser:USER
+                                       withProvider:@"Kidozen"
+                                        andPassword:PASS
+                                         completion:^(id kr)
+        {
             NSAssert(![kr  isKindOfClass:[NSError class]], @"error must be null");
             dispatch_semaphore_signal(semaphore);
         }];
+
     }];
     while (dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW))
         [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:10000]];
